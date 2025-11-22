187b35a03a830bfaeef52b4ed2720ef2e9ac5244
laura <lauravalentinanavas39@gmail.com>
2025-11-22 04:46:40 -0500

    conexion con mongoDB

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
+
 require('dotenv').config();
 const app = require('./app');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 3000;
 
+// Conectar la base de datos
+connectDB();
+
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
